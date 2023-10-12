2193d03083db0989cc6bcafc6fcbb8c627e4b96f
Raf_lgo <144221811+Haretou@users.noreply.github.com>
2023-10-12 10:38:56 +0200

    ghghgh

--- main.go
+++ main.go
@@ -1,67 +1,125 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "math/rand"
-    "time"
+	"fmt"
+	"strings"
+	"math/rand"
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
-    rand.Seed(time.Now().UnixNano())
+	// Initialisation de raylib
+	rl.InitWindow(800, 600, "Hangman Game")
+	rl.SetTargetFPS(60)
 
-    dico := []string{"AVION", "ARBRE", "CAMIONS", "CHEVEUX", "CAMPING", "CHAUSSURE", "CHOUETTE", "LUNETTE"}
-    wordToGuess := dico[rand.Intn(len(dico))] // le mot à deviner
-    guessedWord := make([]string, len(wordToGuess))
+	rand.Seed(time.Now().UnixNano())
 
-    for i := range guessedWord {
-        guessedWord[i] = "_"
-    }
+	dico := []string{"AVION", "ARBRE", "CAMIONS", "CHEVEUX", "CAMPING", "CHAUSSURE", "FOUET", "CHAMPS", "HORLOGE", "CHIPS"} // liste de mot possible
+	wordToGuess := dico[rand.Intn(len(dico))] // le mot à deviner choisi aléatoirement dans la taille du tableau
+	guessedWord := make([]string, len(wordToGuess))
 
-    attemptsLeft := 6
-    guessedLetters := make([]string, 0)
+	for i := range guessedWord {
+		guessedWord[i] = "_"
+	}
 
-    fmt.Println("Welcome to the Hangman game!")
-    fmt.Printf("Guess the word: %s\n", strings.Join(guessedWord, " "))
+	attemptsLeft := 6
+	guessedLetters := make([]string, 0)
 
-    for attemptsLeft > 0 { // Tant qu'il reste des "vies" au joueur
-        fmt.Printf("Attempts left: %d\n", attemptsLeft)
-        fmt.Print("Guessed letters: ")
-        fmt.Println(strings.Join(guessedLetters, " "))
+	for !rl.WindowShouldClose() { // La boucle principale du jeu
 
-        var guess string
-        fmt.Print("Guess a letter: ")
-        fmt.Scanf("%s", &guess)
+		if attemptsLeft == 0 || strings.Join(guessedWord, "") == wordToGuess {
+			break
+		}
 
-        if len(guess) != 1 {
-            fmt.Println("Please enter a single letter.")
-            continue
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Blue)
+
+		// Dessiner l'interface utilisateur
+		rl.DrawText(fmt.Sprintf("Vie restantes : %d", attemptsLeft), 10, 10, 20, rl.Black)
+		rl.DrawText(fmt.Sprintf("Lettres utilisé : %s", strings.Join(guessedLetters, " ")), 10, 40, 20, rl.Black)
+		rl.DrawText(fmt.Sprintf("Mot caché : %s", strings.Join(guessedWord, " ")), 10, 70, 20, rl.Black)
+
+		var guess string // Je code ca comme un golmon mais je sais pas comment le faire autrement ( j'obtimiserais le code plus tard )
+		if rl.IsKeyDown(rl.KeyQ) {
+			guess = "A"
+		} else if rl.IsKeyDown(rl.KeyB) {
+			guess = "B"
+		} else if rl.IsKeyDown(rl.KeyC) {
+            guess = "C"
+        } else if rl.IsKeyDown(rl.KeyD) {
+            guess = "D"
+        } else if rl.IsKeyDown(rl.KeyE) {
+            guess = "E"
+        } else if rl.IsKeyDown(rl.KeyF) {
+            guess = "F"
+        } else if rl.IsKeyDown(rl.KeyG) {
+            guess = "G"
+        } else if rl.IsKeyDown(rl.KeyH) {
+            guess = "H"
+        } else if rl.IsKeyDown(rl.KeyI) {
+            guess = "I"
+        } else if rl.IsKeyDown(rl.KeyJ) {
+            guess = "J"
+        } else if rl.IsKeyDown(rl.KeyK) {
+            guess = "K"
+        } else if rl.IsKeyDown(rl.KeyL) {
+            guess = "L"
+        } else if rl.IsKeyDown(rl.KeyM) {
+            guess = "M"
+        } else if rl.IsKeyDown(rl.KeyN) {
+            guess = "N"
+        } else if rl.IsKeyDown(rl.KeyO) {
+            guess = "O"
+        } else if rl.IsKeyDown(rl.KeyP) {
+            guess = "P"
+        } else if rl.IsKeyDown(rl.KeyA) {
+            guess = "Q"
+        } else if rl.IsKeyDown(rl.KeyR) {
+            guess = "R"
+        } else if rl.IsKeyDown(rl.KeyS) {
+            guess = "S"
+        } else if rl.IsKeyDown(rl.KeyT) {
+            guess = "T"
+        } else if rl.IsKeyDown(rl.KeyU) {
+            guess = "U"
+        } else if rl.IsKeyDown(rl.KeyV) {
+            guess = "V"
+        } else if rl.IsKeyDown(rl.KeyZ) {
+            guess = "W"
+        } else if rl.IsKeyDown(rl.KeyX) {
+            guess = "X"
+        } else if rl.IsKeyDown(rl.KeyY) {
+            guess = "Y"
+        } else if rl.IsKeyDown(rl.KeyW) {
+            guess ="Z"
         }
+		if guess != "" {
+			guess = strings.ToUpper(guess)
 
-        guess = strings.ToUpper(guess) // Convertir la lettre en majuscule pour correspondre au mot en majuscule
+			if strings.Contains(wordToGuess, guess) {
+				for i, letter := range wordToGuess {
+					if string(letter) == guess {
+						guessedWord[i] = guess
+					}
+				}
+			} else {
+				fmt.Printf("La lettre %s n'est pas dans le mot.\n", guess)
+				attemptsLeft--
+			}
 
-        if strings.Contains(wordToGuess, guess) {
-            for i, letter := range wordToGuess {
-                if string(letter) == guess {
-                    guessedWord[i] = guess
-                }
-            }
-        } else {
-            fmt.Printf("The letter %s is not in the word.\n", guess)
-            attemptsLeft--
-        }
+			guessedLetters = append(guessedLetters, guess)
+		}
 
-        guessedLetters = append(guessedLetters, guess)
+		rl.EndDrawing()
+	}
 
-        if strings.Join(guessedWord, "") == wordToGuess {
-            fmt.Println("Congratulations! You have guessed the word:", wordToGuess)
-            break
-        }
+	rl.CloseWindow()
 
-        fmt.Printf("Current word: %s\n", strings.Join(guessedWord, " "))
-    }
-
-    if attemptsLeft == 0 {
-        fmt.Println("You lost! The word was:", wordToGuess)
-    }
+	if attemptsLeft == 0 {
+		fmt.Println("Perdu ! le mot était :", wordToGuess)
+	} else {
+		fmt.Println("GG tu as trouvé le mot:", wordToGuess)
+	}
 }
